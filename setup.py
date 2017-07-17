ba30040ecbcc358f2562151322e544f49d2f95cd
Richard Cornish <richardcornish@gmail.com>
2017-07-17 14:15:04 -0500

    Version bump

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import find_packages, setup
 
 setup(
     name='django-smartspaceless',
-    version='0.1.1',
+    version='0.1.2',
     description='A Django template tag application for minifying block-level HTML elements only',
     long_description=open(os.path.join(os.path.dirname(__file__), 'README.rst')).read(),
     author='Richard Cornish',
